28695dc51768190112a45589b777ffedc73b9677
DMeurer <dom.meurer@gmx.com>
2022-04-24 18:41:08 +0200

    added the Etch-A-Sketch project

---
+++ projekte/etch-a-sketch/sketch.js
@@ -0,0 +1,41 @@
+function generateGrid(size) {
+    const container = document.createElement('div');
+    container.id = "boxContainer";
+
+    var grid = [size];
+    var rows = [size];
+    for (var i = 0; i < size; i++) {
+        grid[i] = [size];
+        rows[i] = document.createElement('div');
+        rows[i].className = 'boxRow';
+        for (var j = 0; j < size; j++) {
+            grid[i][j] = document.createElement('div');
+            grid[i][j].className = 'boxBox';
+            grid[i][j].id = 'box' + i + '-' + j;
+            rows[i].appendChild(grid[i][j]);
+        }
+        container.appendChild(rows[i]);
+    }
+    document.getElementById("content").appendChild(container);
+}
+
+function resetGrid(size) {
+    if (size >= 200) size = 200;
+    document.getElementById("boxContainer").remove();
+    generateGrid(size);
+}
+
+window.onload = function() {
+    generateGrid(16);
+    document.getElementById("reset").onclick = function() {
+        resetGrid(window.prompt("Enter the size of the grid:"));
+    }
+};
+/*
+const step1 = document.createElement('p');
+step1.textContent = "Hey, Im red";
+step1.style.color = "red";
+document.querySelector("#container").appendChild(step1);
+
+document.getElementById("5left").addEventListener("click", function() { rotateRows(5, "left"); });
+*/
